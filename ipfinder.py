91fdd6d270f3cce499e248aa10d757d124b8a212
NumeX <69904588+NumeXx@users.noreply.github.com>
2021-09-17 10:11:58 +0900

    NumeX

---
+++ ipfinder.py
@@ -0,0 +1,13 @@
+# Code By NumeX
+# Kang Col1 (KIPAS)
+
+text = input("Enter Host : ")
+ok = open(text, "r").readlines()
+
+for i in ok:
+    if i[0].startswith("#"):
+        pass
+    elif not i[0].isdigit():
+        pass
+    else:
+        print(i) 
