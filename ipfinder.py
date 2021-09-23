49bf3b1cfa11f8ed1334232e5d48285cd5128bd8
NumeX <69904588+NumeXx@users.noreply.github.com>
2021-09-24 02:28:33 +0900

    Update ipfinder.py

--- ipfinder.py
+++ ipfinder.py
@@ -5,9 +5,9 @@ text = input("Enter Host : ")
 ok = open(text, "r").readlines()
 
 for i in ok:
-    if i[0].startswith("#"):
-        pass
-    elif not i[0].isdigit():
-        pass
-    else:
-        print(str(i)) 
+	if i[0].startswith("#"):
+		pass
+	elif not i[0].isdigit():
+		pass
+	else:
+		print(i)
